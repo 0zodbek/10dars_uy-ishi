4f296d32db0710c637d0e928c7525f5094d1a693
0zodbek <ozpdbekov@gmail.com>
2024-07-07 16:27:33 +0500

    g

--- js/main.js
+++ js/main.js
@@ -1,11 +1,13 @@
 import { getData,createCard } from "./functions.js"
-
+const header = document.querySelector('header')
 const wrapper = document.querySelector('#wrapper')
-
+const loader =document.querySelector('#loader')
 document.addEventListener('DOMContentLoaded',function(){
 getData("https://strapi-store-server.onrender.com/api/products")
 .then(data=> {
 data.data.length > 0 && data.data.forEach(el => {
+    wrapper.style.display = 'flex'
+    header.style.display = 'flex'
     let card = createCard(el);
     wrapper.innerHTML += card;
     
@@ -14,4 +16,7 @@ data.data.length > 0 && data.data.forEach(el => {
 .catch(err=>{
 console.log(err);
 })
+.finally(function(){
+    loader.style.display = 'none'
+    })
 })
